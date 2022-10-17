144b7e39910d35d4db1886973dcb9b8d3d65f3ad
MiguelGarcia98Github <miguelgarciard98@gmail.com>
2022-10-17 15:27:55 +0200

    Tests not working with TS, TS and Jest configurated properly

---
+++ dist/tools.test.js
@@ -0,0 +1,9 @@
+import { double } from './tools';
+describe('first', () => {
+    test('should first', () => {
+        const x = 2;
+        const expectedR = 4;
+        const r = double(x);
+        expect(r).toBe(expectedR);
+    });
+});
